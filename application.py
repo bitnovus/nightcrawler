717a874fb14e501f9674acf3130870efab14b325
Daniel <dchyan1@gmail.com>
2013-04-09 15:24:55 -0400

    fixed something?

--- application.py
+++ application.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import os
+
+#=========================================================
 
 application = Flask(__name__)
 
 #Set application.debug=true to enable tracebacks on Beanstalk log output. 
 #Make sure to remove this line before deploying to production.
 application.debug=True
+
+application.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') \
+        if os.environ.get('DATABASE_URL') else 'sqlite:///test.db'
+
+#=========================================================
 
 @application.route('/')
 def landing_page(name=None):
@@ -17,6 +25,8 @@ def home_page(name=None):
 @application.route('/timeline')
 def timeline(name=None):
     return render_template('timeline.html', name=name)
+
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
