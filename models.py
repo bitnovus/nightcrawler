be7e8039b3e0099a296852a7b22241d92c1759e0
Daniel <dchyan1@gmail.com>
2013-04-11 00:20:06 -0400

    missing import

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') \
