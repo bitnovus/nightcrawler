3983402882513a162565584f85af13e97696e31d
Katherine Pogrebniak <katherinepogrebniak@nat-oitwireless-inside-vapornet100-c-6992.princeton.edu>
2013-04-21 21:20:22 -0400

    megabus updated

--- flights.py
+++ flights.py
@@ -33,15 +33,27 @@ def sanitize_times(times):
     g.append(s)
   return g
 
-def package_info(times, prices, carriers, start, end):
+def get_hour(time):
+  hour = int(time[0:time.find(':')])
+  if time[time.find(':') + 4] == 'P' or hour==12 or hour==1:
+    hour = hour + 12
+  return hour
+
+def get_min(time):
+  colon = time.find(':')
+  return int(time[colon + 1: colon + 3])
+
+def package_info(times, prices, carriers, start, end, hour, minute, isArriving):
   i = len(prices)
   dictionaries = []
   for j in range(len(prices)):
-    dictionaries.append(dict(price = prices[j], departure = start, arrival = end, departure_time = times[2 * j], arrival_time = times[2 * j+ 1], carrier = carriers[j]))
+    if isArriving and (get_hour(times[2*j+1]) < hour or (get_hour(times[2*j+1]) == hour and get_min(times[2*j+1]) <= minute)):
+      dictionaries.append(dict(price = prices[j], departure = start, arrival = end, departure_time = times[2 * j], arrival_time = times[2 * j+ 1], carrier = carriers[j]))
+    elif isArriving is False and (get_hour(times[2*j]) > hour or (get_hour(times[2*j]) == hour and get_min(times[2*j])>= minute)):
+        dictionaries.append(dict(price = prices[j], departure = start, arrival = end, departure_time = times[2 * j], arrival_time = times[2 * j+ 1], carrier = carriers[j]))
   return dictionaries
 
-
-def orbitz(start, end, month, day, year):
+def orbitz(start, end, month, day, year, hour, minute, isArriving):
   s = 'http://www.orbitz.com/shop/home?type=air&ar.type=oneWay&ar.ow.leaveSlice.orig.dl=&ar.ow.leaveSlice.dest.dl=&ar.ow.leaveSlice.orig.key='+ str(start) + '&_ar.ow.leaveSlice.originRadius=0&ar.ow.leaveSlice.dest.key='+ str(end) + '&_ar.ow.leaveSlice.destinationRadius=0&ar.ow.leaveSlice.date='+ str(month) + '%2F' + str(day) + '%2F' + str(year) + '&ar.ow.leaveSlice.time=Anytime&ar.ow.numAdult=1&ar.ow.numSenior=0&ar.ow.numChild=0&ar.ow.child%5B0%5D=&ar.ow.child%5B1%5D=&ar.ow.child%5B2%5D=&ar.ow.child%5B3%5D=&ar.ow.child%5B4%5D=&ar.ow.child%5B5%5D=&ar.ow.child%5B6%5D=&ar.ow.child%5B7%5D=&_ar.ow.nonStop=0&_ar.ow.narrowSel=0&ar.ow.narrow=airlines&ar.ow.carriers%5B0%5D=&ar.ow.carriers%5B1%5D=&ar.ow.carriers%5B2%5D=&ar.ow.cabin=C&search=Search+Flights'
 
   html = urllib2.urlopen(s).read()
@@ -54,7 +66,9 @@ def orbitz(start, end, month, day, year):
   carriers = re.findall(".* </strong>", html)
   carriers_s = sanitize_carriers(carriers)
 
-  return package_info(times_s, prices_s, carriers_s, start, end)
+  p = package_info(times_s, prices_s, carriers_s, start, end, hour, minute, isArriving)
+  print p
+  return p
 
 if __name__ == '__main__':
-    orbitz("EWR", "MIA", 4, 14, 2013)
+    orbitz("EWR", "MIA", 4, 14, 2013, 13, 30, True)
--- megabus.py
+++ megabus.py
@@ -3,52 +3,64 @@
 import urllib2, re
 
 def sanitize_times(times):
-	g = []
-	for t in times:
-		g.append(re.sub(r"\xc2\xa0", " ", t))
-	return g
+  g = []
+  for t in times:
+    g.append(re.sub(r"\xc2\xa0", " ", t))
+  return g
 
 def sanitize_departures(departures):
-	g = []
-	for d in departures:
-		s = re.sub(r"ALBACORE\t\t\t\t\t\t\t\t", "", d)
-		s = re.sub("\rAARDVARK", "", s)
-		g.append(s)
-	return g
+  g = []
+  for d in departures:
+    s = re.sub(r"ALBACORE\t\t\t\t\t\t\t\t", "", d)
+    s = re.sub("\rAARDVARK", "", s)
+    g.append(s)
+  return g
 
-def package_info(times, prices, departures):
-	dictionaries = []
-	for j in range(len(prices)):
-		dictionaries.append(dict(price = prices[j], departure = departures[2 * j], arrival = departures[2 * j + 1], departure_time = times[2 * j], arrival_time = times[2 * j+ 1], carrier = "Megabus"))
-	return dictionaries
+def get_hour(time):
+  hour = int(time[0:time.find(':')])
+  if time[time.find(':') + 4] == 'P' or hour==12 or hour==1:
+    hour = hour + 12
+  return hour
 
+def get_min(time):
+  colon = time.find(':')
+  return int(time[colon + 1: colon + 3])
 
-def megabus(start, end, month, day, year):
-	s = "https://us.megabus.com/JourneyResults.aspx?originCode=" + str(start) + "&destinationCode=" + str(end) + "&outboundDepartureDate=" + str(month) + "%2f" + str(day) + "%2f" + str(year) + "&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=0"
-	html = urllib2.urlopen(s).read()
+def package_info(times, prices, departures, hour, minute, isArriving):
+  dictionaries = []
+  for j in range(len(prices)):
+    if isArriving and (get_hour(times[2*j+1]) < hour or (get_hour(times[2*j+1]) == hour and get_min(times[2*j+1]) <= minute)):
+      dictionaries.append(dict(price = prices[j], departure = departures[2*j], arrival = departures[2*j+1], departure_time = times[2*j], arrival_time = times[2*j+1], carrier = "Megabus"))
+    elif isArriving is False and (get_hour(times[2*j]) > hour or (get_hour(times[2*j]) == hour and get_min(times[2*j]) >= minute)):
+      dictionaries.append(dict(price = prices[j], departure = departures[2*j], arrival = departures[2*j+1], departure_time = times[2*j], arrival_time = times[2*j+1], carrier = "Megabus"))
+  return dictionaries
 
-	times = re.findall("[0-9]*[0-9]:[0-9][0-9].*[AP]M", html)
-	times_s = sanitize_times(times)
+def megabus(start, end, month, day, year, hour, minute, isArriving):
+  s = "https://us.megabus.com/JourneyResults.aspx?originCode=" + str(start) + "&destinationCode=" + str(end) + "&outboundDepartureDate=" + str(month) + "%2f" + str(day) + "%2f" + str(year) + "&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=0"
+  html = urllib2.urlopen(s).read()
 
-	prices = re.findall("\$[0-9]*\.[0-9][0-9]", html)
-	lines = html.split("\n")
-	s = ""
-	next = False
-	for line in lines:
-		if next:
-			s = s + "ALBACORE" + line + "AARDVARK \n"
-			next = False
-		else:
-			s = s + line + " "
-		if ",<!-- mp_trans -->" in line:
-			next = True
+  times = re.findall("[0-9]*[0-9]:[0-9][0-9].*[AP]M", html)
+  times_s = sanitize_times(times)
 
-	departures = re.findall("ALBACORE.*AARDVARK", s)
-	departures_s = sanitize_departures(departures)
+  prices = re.findall("\$[0-9]*\.[0-9][0-9]", html)
+  lines = html.split("\n")
+  s = ""
+  next = False
+  for line in lines:
+    if next:
+      s = s + "ALBACORE" + line + "AARDVARK \n"
+      next = False
+    else:
+      s = s + line + " "
+    if ",<!-- mp_trans -->" in line:
+      next = True
 
-	p = package_info(times_s, prices, departures_s)
-	#print p
-	return p
+  departures = re.findall("ALBACORE.*AARDVARK", s)
+  departures_s = sanitize_departures(departures)
+
+  p = package_info(times_s, prices, departures_s, hour, minute, isArriving)
+  print p
+  return p
 
 if __name__ == '__main__':
-    megabus(302, 289, 4, 14, 2013)
+    megabus(89, 123, 4, 20, 2013, 13, 30, False)
