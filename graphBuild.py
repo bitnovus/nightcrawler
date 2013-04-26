cfb17042ad4274d0e5e43bd12258897c9ae9681e
Ubuntu <ubuntu@ip-10-243-150-141.ec2.internal>
2013-04-26 21:43:30 +0000

    removed mysql requirement

--- graphBuild.py
+++ graphBuild.py
@@ -9,11 +9,12 @@ app= Flask(__name__)
 
 #=========================================================
 
-def build_graph_for_date(db, day, month, year):
+def build_graph_for_date(db, cities, month, day, year):
     for city1 in cities:
         for city2 in cities:
             if city1 != city2:
                 print city1.name + " " + city2.name
+                print megabus(city1.megacode, city2.megacode, month, day, year, 0, 0, False)
 
 
 def main():
@@ -23,6 +24,7 @@ def main():
 
     app.config.from_object(graphConfiguration.Config())
     db = SQLAlchemy(app)
+    build_graph_for_date(db, cities, 4, 26, 2013)
                 
 
 if __name__ == "__main__":
