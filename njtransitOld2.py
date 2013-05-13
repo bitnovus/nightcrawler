d91501c6101ec2a185579d04d8b74614406a2666
Katherine Pogrebniak <katherinepogrebniak@nat-oitwireless-inside-vapornet100-c-6992.princeton.edu>
2013-05-13 19:45:21 -0400

    all orbitz flights

---
+++ njtransitOld2.py
@@ -0,0 +1,67 @@
+import urllib2
+import urllib
+import re
+
+#r = requests.post("http://www.njtransit.com/sf/sf_servlet.srv?hdnPageAction=TrainSchedulesFrom", params=payload)
+#curl -d "selOrigin=124_PRIN&selDestination=125_NEC&datepicker=4%2F26%2F2013&OriginDescription=Princeton&DestDescription=Princeton+Junction" http://www.njtransit.com/sf/sf_servlet.srv?hdnPageAction=TrainSchedulesFrom 
+
+def comp_times(time, hour, min):
+  hour2 = int(get_hour(time))
+  if time[time.find(':') + 4]=='P' and hour2!=12:
+  	hour2 += 12
+  if time[time.find(':') + 4] == 'A' and hour2==12:
+  	hour2 -= 12
+  min2 = int(get_min(time))
+  if ((hour2< int(hour)) or (hour2 == int(hour) and min2 < int(min))):
+  	return True
+  return False
+
+def get_hour(time):
+  hour = int(time[0:time.find(":")])
+  return hour
+
+def get_min(time):
+  colon = time.find(':')
+  return int(time[colon+1: colon+3])
+
+def package_info(times, price2, start, end, hour, minute, isArriving, link, payload):
+  dictionaries = []
+  for j in range(len(times)/2):
+  	if isArriving and (comp_times(times[2*j], hour, minute) and comp_times(times[2*j+1], hour, minute)):
+  		dictionaries.append(dict(price = price2, departure = start, arrival = end, departure_time = times[2*j], arrival_time = times[2*j+1], carrier = "NJ Transit", link = link, payload = payload))
+  	elif ((isArriving is False) and ((comp_times(times[2*j], hour, minute) is False))):
+  		dictionaries.append(dict(price = price2, departure = start, arrival = end, departure_time = times[2*j], arrival_time = times[2*j+1], carrier = "NJ Transit", link = link, payload = payload))
+  return dictionaries 
+
+def sanitize_loc(loc):
+	r = loc.replace('+', ' ')
+	return r
+
+def njtransit(start, end, codeStart, codeEnd, month, day, year, hour, min, isArriving):
+
+  date = str(month) + '%2F' + str(day) + '%2F' + str(year)
+  payload = {'selOrigin': codeStart, 
+  'selDestination': codeEnd, 
+  'datepicker' : date, 
+  'OriginDescription' : start, 
+  'DestDescription' : end }
+
+  link = "http://www.njtransit.com/sf/sf_servlet.srv?hdnPageAction=TrainSchedulesFrom"
+
+  req = urllib2.Request(link, urllib.urlencode(payload))
+  res = urllib2.urlopen(req)
+  html = res.read()
+  #print html
+
+  times = re.findall("[0-9]*[0-9]:[0-9][0-9].*[AP]M", html)
+  price = re.search("\$[0-9]*\.[0-9][0-9]", html).group(0)
+  start = sanitize_loc(start)
+  end = sanitize_loc(end)
+
+  p = package_info(times, price, start, end, hour, min, isArriving, link, urllib.urlencode(payload))
+  #print p
+  return p
+
+
+if __name__ == '__main__':
+	print njtransit('Newark+Airport', 'Princeton+Junction', '37953_NEC', '125_NEC', '5', '9', '2013', '10', '30', False)
